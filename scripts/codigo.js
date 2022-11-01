0312cb0a29f9cb2e107c6acb246311f49fa80721
Franco Mai <francoomai18@gmail.com>
2022-11-01 19:08:19 -0300

    conversor de monedas terminado

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -1,55 +1,49 @@
-function convertir(){
-    var valor = parseFloat(document.getElementById("cantidad").value);
-    document.getElementById("valor").innerHTML="<b>"+ valor + "<b>";
-    de=document.getElementById("de").value;
-    a=document.getElementById("a").value;
-    var dolar = 292.00
-    var euro = 270.00
-    var resultado = 0
+function convertir() {
+    let valor = parseFloat(document.getElementById("cantidad").value);
+    de = document.getElementById("de").value;
+    a = document.getElementById("a").value;
+
+    let dolar = 292.00
+    let euro = 270.00
+    let resultado = 0
 
     //peso a dolar
-    if(de==1&&a==2)
-    {
-        resultado=valor/dolar;
+    if (de == 1 && a == 2) {
+        resultado = valor / dolar;
     }
 
     //peso a euro
-    else if(de==1&&a==3)
-    {
-        resultado=valor/euro;
+    else if (de == 1 && a == 3) {
+        resultado = valor / euro;
     }
 
     //dolar a peso
-    else if(de==2&&a==1)
-    {
-        resultado=valor*dolar;
+    else if (de == 2 && a == 1) {
+        resultado = valor * dolar;
     }
 
     //dolar a euro
-    else if(de==2&&a==3)
-    {
-        resultado=valor*(dolar/euro);
+    else if (de == 2 && a == 3) {
+        resultado = valor * (dolar / euro);
     }
 
     //euro a peso
-    else if(de==3&&a==1)
-    {
-        resultado=valor*euro;
+    else if (de == 3 && a == 1) {
+        resultado = valor * euro;
     }
 
     //euro a dolar
-    else if(de==3&&a==2)
-    {
-        resultado=valor*(euro/dolar);
+    else if (de == 3 && a == 2) {
+        resultado = valor * (euro / dolar);
     }
 
     //peso a peso, dolar a dolar, euro a euro
-    else
-    {
-        resultado=valor;
+    else {
+        resultado = valor;
     }
-    document.getElementById("resultado").innerHTML="Resultado: $" + resultado.toFixed(2);
-     
+    document.getElementById("resultado").innerHTML = "Resultado: $" + resultado.toFixed(2);
+    botonconvertir.addEventListener('click', convertir)
+
 }
 
 
